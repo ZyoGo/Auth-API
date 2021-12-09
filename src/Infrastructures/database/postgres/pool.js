03d2a0934bb69d49fe73ac4a8a52fcf1279367f1
Harly <academyynetworking@gmail.com>
2021-12-10 00:06:34 +0800

    Create pool database

---
+++ src/Infrastructures/database/postgres/pool.js
@@ -0,0 +1,15 @@
+/* istabnul ignore file */
+
+const { Pool } = require('pg');
+
+const testConfig = {
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+};
+
+const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
+
+module.exports = pool;
