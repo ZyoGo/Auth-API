6319e12aa7bf7209e561f8bfe437c0e244c2fe7b
Harly <academyynetworking@gmail.com>
2021-12-11 21:21:52 +0800

    Update add testing error server

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -144,5 +144,28 @@ describe('HTTP server', () => {
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message).toEqual('username tidak tersedia');
     });
+
+    it('should handler server error correctly', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'dicoding',
+        fullname: 'Dicoding Indonesia',
+        password: 'super_secret',
+      };
+      const server = await createServer({});
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(500);
+      expect(responseJson.status).toEqual('error');
+      expect(responseJson.message).toEqual('terjadi kegagalan pada server kami');
+    });
   });
 });
